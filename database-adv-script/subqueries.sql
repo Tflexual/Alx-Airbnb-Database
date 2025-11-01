f8c3b056e3646ec954e0f66ff8b60091e49938a8
Odelola oluwatobi <129215773+Tflexual@users.noreply.github.com>
2025-11-01 12:42:08 +0100

    Update subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,1 +1,34 @@
+1. INNER JOIN – show bookings with user details :
+
+-- INNER JOIN: bookings + users
+SELECT 
+  bookings.id AS booking_id,
+  users.name AS user_name,
+  bookings.property_id
+FROM bookings
+INNER JOIN users
+  ON bookings.user_id = users.id;
+
+
+2. LEFT JOIN – show all properties and their reviews
+
+
+-- LEFT JOIN: properties + reviews
+SELECT 
+  properties.name AS property_name,
+  reviews.rating
+FROM properties
+LEFT JOIN reviews
+  ON properties.id = reviews.property_id;
+
+
+3. FULL OUTER JOIN – show all users and all bookings
+
+-- FULL OUTER JOIN: users + bookings
+SELECT 
+  users.name AS user_name,
+  bookings.id AS booking_id
+FROM users
+FULL OUTER JOIN bookings
+  ON users.id = bookings.user_id;
 
